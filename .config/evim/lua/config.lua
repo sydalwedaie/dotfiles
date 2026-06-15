dc2af1f4f69e79fd9756b8b0b03fd032c36d5192
Sayed Alwedaie <5216479+sydalwedaie@users.noreply.github.com>
2026-06-15 12:26:30 +0300

    Modify alacritty.

--- .config/evim/lua/config.lua
+++ .config/evim/lua/config.lua
@@ -41,7 +41,7 @@ require("nvim-treesitter").install(parsers)
 vim.diagnostic.config({
 	update_in_insert = false,
 	severity_sort = true,
-	float = { border = "rounded", source = "if_many" },
+	float = { border = "none", source = "if_many" },
 	underline = { severity = { min = vim.diagnostic.severity.WARN } },
 
 	virtual_text = true,
